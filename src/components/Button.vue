c794efc374784f0b4a1b34dba6f4df36261b76a3
jon13002 <jonmtz3002@gmail.com>
2024-05-28 15:09:29 -0600

    ♻Refactor: Component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,11 +3,23 @@ import { RouterLink } from 'vue-router';
 
 defineProps({
     label:  String,
+    actionLabel: {
+        default: 'Crear'
+    },
     name: String,
     width: {
         type: String,
         default: 'w-40'
-    }
+    },
+    icon: {
+        default: 'bi bi-plus-lg'
+    },
+    background: {
+        default: 'bg-green-500'
+    },
+    borderColor: {
+        default: 'border-green-500'
+    },
 })
 
 </script>
@@ -15,16 +27,16 @@ defineProps({
 <template>
     <RouterLink
         :to="{ name: name }"
-        :class="`rounded-lg relative ${width} h-10 cursor-pointer flex items-center border border-green-500 bg-green-500 group hover:bg-green-500 active:bg-green-500 active:border-green-500`"
+        :class="`rounded-lg relative ${width} h-10 cursor-pointer flex items-center border ${borderColor} ${background} group`"
     >
         <span
             class="text-gray-200 font-semibold ml-6 transform group-hover:translate-x-10 group-hover:opacity-0 transition-all duration-300 ease-in-out text-xs">
-            Crear {{ label }}
+            {{ actionLabel }} {{ label }}
         </span>
 
         <span
-            class="absolute right-0 h-full w-10 rounded-lg bg-green-500 flex items-center justify-center transform group-hover:translate-x-0 group-hover:w-full transition-all duration-300">
-            <i class="bi bi-plus-lg text-white"></i>
+            :class="`absolute right-0 h-full w-10 rounded-lg ${background} flex items-center justify-center transform group-hover:translate-x-0 group-hover:w-full transition-all duration-300`">
+            <i :class="`${icon} text-gray-200`"></i>
         </span>
     </RouterLink>
 </template>
