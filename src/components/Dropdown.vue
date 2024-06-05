8b9c1438030cce02a37768b2edab00713e3525ee
jon13002 <jonmtz3002@gmail.com>
2024-06-05 11:05:09 -0600

    ✨Feat: Agregando componente

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn rounded-full m-1 bg-gray-300 dark:bg-[#14191E]">
+            <i class="bi bi-three-dots rotate-90"></i>
+        </div>
+
+        <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-gray-300 dark:bg-[#14191E] rounded-box w-52 gap-2">
+            <slot />
+        </ul>
+    </div>
+</template>
