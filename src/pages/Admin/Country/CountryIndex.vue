d9f030c77fc437337eeff948a29d75d54803a923
jon13002 <jonmtz3002@gmail.com>
2024-07-15 13:30:42 -0600

    ♻Refactor: Countries service

--- src/pages/Admin/Country/CountryIndex.vue
+++ src/pages/Admin/Country/CountryIndex.vue
@@ -1,22 +1,77 @@
-<script setup>
-import { defineAsyncComponent, onMounted, ref } from "vue";
-import { useCountriesStore } from "../../../store/Admin/countries";
+<script>
+import { onMounted, ref } from "vue";
+import country from '../../../services/country.service';
 
 import Button from "../../../components/Button.vue";
-import TableRow from "../../../components/TableRow.vue";
-import TableHeaderCell from "../../../components/TableHeaderCell.vue";
+import Pagination from "../../../components/Pagination.vue";
+import Table from '../../../components/Table.vue';
 import TableDataCell from "../../../components/TableDataCell.vue";
-const Table = defineAsyncComponent(() =>
-    import('../../../components/Table.vue')
-);
+import TableHeaderCell from "../../../components/TableHeaderCell.vue";
+import TableRow from "../../../components/TableRow.vue";
 
-const store = useCountriesStore();
-const countries = ref([]);
+export default {
+    setup() {
+        const countries = ref({});
+        const currentPage = ref(1);
+        const totalPages = ref(1);
 
-onMounted(async () => {
-    await store.getAll();
-    countries.value = store.countries;
-});
+        const fetchCountries = async (page) => {
+            const { data } = await country.getAll(page);
+
+            countries.value = data.data;
+            currentPage.value = data.meta.currentPage;
+            totalPages.value = data.meta.totalPages;
+        };
+
+        const handlePageChange = (page) => {
+            fetchCountries(page);
+        };
+
+        onMounted(async () => {
+            fetchCountries();
+        });
+
+        return {
+            countries,
+            currentPage,
+            totalPages,
+            handlePageChange,
+            fetchCountries,
+        };
+    },
+    methods: {
+        confirmDelete(countryId) {
+            this.$swal.fire({
+                title: '¿Estás seguro de eliminar este registro?',
+                text: "¡No podrás revertir esto!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '758694',
+                confirmButtonText: 'Eliminar',
+                cancelButtonText: 'Cancelar',
+            }).then(async (result) => {
+                if (result.isConfirmed) {
+                    await country.delete(countryId);
+                    this.$swal.fire(
+                        '¡Eliminado!',
+                        'El registro ha sido eliminado.',
+                        'success'
+                    );
+                    await this.fetchCountries(this.currentPage);
+                }
+            });
+        },
+    },
+    components: {
+        Button,
+        TableRow,
+        TableHeaderCell,
+        TableDataCell,
+        Table,
+        Pagination,
+    },
+};
 
 </script>
 
@@ -41,16 +96,29 @@ onMounted(async () => {
             <template #content>
                 <TableRow v-for="country in countries" :key="country.id">
                     <TableDataCell>{{ country.name }}</TableDataCell>
-                    <TableDataCell>
+                    <TableDataCell class="flex gap-2 text-center">
 
                         <router-link :to="{ path: '/admin/countries/' + country.id + '/edit/'}"
-                            class="py-2 px-4 text-black bg-amber-400 hover:bg-amber-500 rounded-md duration-200">
+                            class="btn text-black bg-amber-400 hover:bg-amber-500">
                             <i class="bi bi-pencil-square"></i>
                         </router-link>
+
+                        <button 
+                            class="btn bg-red-500 hover:bg-red-600 text-white"
+                            @click="confirmDelete(country.id)"
+                        >
+                            <i class="bi bi-trash3-fill"></i>
+                        </button>
 
                     </TableDataCell>
                 </TableRow>
             </template>
         </Table>
+
+        <Pagination 
+            :currentPage="currentPage" 
+            :totalPages="totalPages" 
+            @page-changed="handlePageChange" 
+        />
     </section>
 </template>
